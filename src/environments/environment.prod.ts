6845f09853cae68225836e310abc9129acf61bbd
Samson K Okemakinde <csamsonok@gmail.com>
2025-03-13 12:07:02 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,7 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  baseUrl: "http://127.0.0.1:5000"
+  // baseUrl: "http://127.0.0.1:5000"
+  baseUrl: "https://icebreakerai-r3tl7.ondigitalocean.app/",
+  googleClientId: "802638642148-l9humukverb1prjp5ecvcc17fenp18vs.apps.googleusercontent.com",
 };
